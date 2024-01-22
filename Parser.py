7303939922e1a8cc6d630e11f6c0f2de91f7acb0
Zhipeng Zhang <zzp_1988@163.com>
2024-01-22 10:47:25 +0800

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -55,7 +55,7 @@ def parse_args():
 
     args = parser.parse_args()
 
-    save_dir = 'trained_model/KGRL/{}/embed-dim{}_relation-dim{}_{}_{}_lr{}_pretrain{}/'.format(
+    save_dir = 'trained_model/KGNS/{}/embed-dim{}_relation-dim{}_{}_{}_lr{}_pretrain{}/'.format(
         args.dataset, args.embedding_dim, args.relation_dim, args.laplacian_type,
         '-'.join([str(i) for i in eval(args.conv_dim_list)]), args.lr, args.use_pretrain)
     args.save_dir = save_dir
